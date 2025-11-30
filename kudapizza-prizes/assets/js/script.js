6f860c42b3e8cd08f605c948cd80fe8b26cc9346
Kirill Shumskiy <kirillshumsky7@gmail.com>
2025-11-30 12:02:43 +0500

    Добавлен popup фильтр

--- kudapizza-prizes/assets/js/script.js
+++ kudapizza-prizes/assets/js/script.js
@@ -8,3 +8,30 @@ document.querySelector('.prizes__button').addEventListener('click', function () 
     this.textContent = textButtonTime;
     document.querySelector('.prizes__content-text').classList.toggle('prizes__content-text--active');
 });
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    const buttonPopup = document.querySelector('.catalog__sort');
+    const buttonClosePopup = document.querySelector('.popup-filter__header-button');
+    const popupFilter = document.querySelector('.popup-filter');
+    //open
+    if(buttonPopup){
+        buttonPopup.addEventListener('click', function(){
+            popupFilter.classList.add('popup-filter--show');
+            setTimeout(function(){
+                popupFilter.classList.add('popup-filter--active');
+                popupFilter.classList.add('.no-scroll')
+            }, 100);
+        });
+    }
+
+    //close
+    if(buttonClosePopup){
+        buttonClosePopup.addEventListener('click', function(){
+            popupFilter.classList.remove('popup-filter--active');
+            setTimeout(function(){
+                popupFilter.classList.remove('popup-filter--show');
+            }, 100);
+        })
+    }
+});
